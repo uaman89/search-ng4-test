e6eaeece0e74d42672244a4a9883f80aa29a3725
uaman@gmail.com <git100500>
2017-11-11 14:49:40 +0200

    feat: implement base functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import {Component} from '@angular/core';
+import {URLSearchParams} from '@angular/http';
+
+interface SearchSystem {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,15 +14,41 @@ import {Component} from '@angular/core';
 export class AppComponent {
   public title = 'app works!';
   public query: string;
+  public searchSystem: SearchSystem;
+  public appError: string;
 
-  public searchServiceList: any[] = [
+  public searchSystemList: SearchSystem[] = [
     {name: 'Google', url: 'https://www.google.com.ua/search?q='},
     {name: 'Bing', url: 'https://www.bing.com/search?q='},
     {name: 'Ask', url: 'https://www.ask.com/web?q='},
+    {name: 'Broken', url: 'lalala'},
   ];
-  public error: string;
+
 
   public onSearchClick() {
 
+    // validate selected search:
+    try {
+      if (!/^http(s?):\/\/w+?/.test(this.searchSystem.url)) {
+        throw Error('invalid url of SearchSystem!');
+      }
+    } catch (error) {
+      console.error(` at onSearchClick():`, error);
+      this.appError = error;
+    }
+
+    console.log(`this.query:`, this.query);
+    // validate query:
+    if (!this.query) {
+      this.appError = 'Enter a search query!';
+    }
+
+    if (this.appError) {
+      return;
+    }
+
+    // go to search result:
+    const href = this.searchSystem.url + encodeURI(this.query);
+    window.location.href = href;
   }
 }
